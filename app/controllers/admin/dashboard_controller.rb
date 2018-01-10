b8dedd61048a9f95c34e86007719010dbb394afd
Erika Smith <erikaannesmith@gmail.com>
2018-01-09 19:04:37 -0700

    use partials as refactor

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,9 +1,22 @@
 class Admin::DashboardController < ApplicationController
+  before_action :set_partial, only: :show
 
   def show
     @designs = Design.where.not(status: 'pending')
     @pending_designs = Design.where(status: 'pending') 
     @orders = Order.all 
+  end
+
+  private
+
+  def set_partial
+    default_partials = Hash.new("all")
+    valid_partials = {
+      "pending" => "pending",
+      "orders" => "orders"
+    }
+    page = params[:page]
+    @partial = default_partials.merge(valid_partials)[page]
   end
 
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,21 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_partial, only: :show
 
   def show
     @user = User.find(params[:format])
     @designer_orders = Order.where(designer_id: @user.id)
     @producer_orders = Order.where(producer_id: @user.id)   
+  end
+
+  private
+
+  def set_partial
+    default_partials = Hash.new('orders')
+    valid_partials = {
+      'other' => 'other'
+    }
+    page = params[:page]
+    @partial = default_partials.merge(valid_partials)[page]
   end
 
 end
