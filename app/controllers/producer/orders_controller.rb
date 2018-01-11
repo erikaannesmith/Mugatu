905228bfb36776c9b097356d56d09b1afb39a9fb
Erika Smith <erikaannesmith@gmail.com>
2018-01-11 12:49:44 -0700

    full functionality locally

--- app/controllers/producer/orders_controller.rb
+++ app/controllers/producer/orders_controller.rb
@@ -7,6 +7,7 @@ class Producer::OrdersController < ApplicationController
   def ship
     @order = Order.find(params[:format])
     @order.shipped!
+    @order.design.inactive!
     redirect_to producer_order_path(current_user, @order)
     flash[:success] = "Order ##{@order.id} has been marked as 'shipped'."
   end
